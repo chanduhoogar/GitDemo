9af9ed5e1d27a225ec62d058dca0fb092bcae141
Chandana M <Chandana.M@visionetsystems.com>
2021-11-11 11:27:05 +0530

    Add project files.

---
+++ GitDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 23;
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("the given number is even");
+
+            }
+            else
+            {
+                Console.WriteLine("the given number is odd");
+            }
+        }
+    }
+}
